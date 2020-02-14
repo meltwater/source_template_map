24c678bae6fe478662cf450a338a425146c38094
MoritzLoreyMW <35338716+MoritzLoreyMW@users.noreply.github.com>
2020-02-14 14:46:02 +0100

    Update source_template_map.py

--- source_template_map.py
+++ source_template_map.py
@@ -3,7 +3,7 @@ from mysql_client.sql_client import sql_worker
 import datetime
 import time
 
-spreadsheet_id = '1EUEjnOcMxU8cerQjHSZhJMkp_D5pcTdOqQ12DsuMIOA'
+spreadsheet_id = '1fnLuniqzygD_zW1CGRmU9XGaBnm14wRmc6vUHWy-PbQ'
 
 query = 'SELECT template_file, CONCAT("https://tkrobotng.meltwater.net/tkrobotNG/index.asp?file=", REPLACE(template_file, ".cfg", ""), "&view=current&page=2") as template_link, ssi.sourceid, name, url, category, country, language, active, last_updated, CONCAT("https://tkrobotng.meltwater.net/tkrobotNG/index.asp?id=", ssi.sourceid, "&view=current&page=2") as source_link  FROM (SELECT sourceid, extra_info as template_file FROM source_extra_info WHERE type="inherit") as sei JOIN (SELECT sourceid, name, url, category, country, language FROM source_simplified_info WHERE sourceid IN (SELECT sourceid FROM source_extra_info WHERE type="inherit")) as ssi ON sei.sourceid=ssi.sourceid JOIN (SELECT sourceid, MAX(update_time) as last_updated FROM source_update_history WHERE sourceid IN (SELECT sourceid FROM source_extra_info WHERE type="inherit") GROUP BY 1) as suh ON sei.sourceid=suh.sourceid JOIN (SELECT sourceid, extra_info as active FROM source_extra_info WHERE type="activated" AND sourceid IN (SELECT sourceid as template_file FROM source_extra_info WHERE type="inherit")) as act ON act.sourceid=suh.sourceid ORDER BY 1'
 
